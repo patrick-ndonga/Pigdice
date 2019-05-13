0d488983f50ee81aa5af1353de156b09cf8e96ca
Patrick Ndonga <rick.ndonga.com>
2019-05-13 23:59:07 +0300

    adding content in .js file

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,12 @@ $(document).ready(function(){
     $(".hide2").fadeIn();
     $(".clause2").fadeIn();
   });
+
+  var name1 , accrueRoll1, total1,  name2 , accrueRoll2, total2  ;
+
+ total1 = 0;
+ accrueRoll1 = 0;
+ total2 = 0;
+ accrueRoll2 = 0;
+
+ 
