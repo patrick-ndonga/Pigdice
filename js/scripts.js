3e020672ca646d581f9e0c47dd35dac52152a7c6
Patrick Ndonga <rick.ndonga.com>
2019-05-13 23:25:44 +0300

    Addition of .html content

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#play1").click(function(){
+    $(".hide1").fadeIn();
+    $(".para1").fadeIn();
+  });
+  $("#play2").click(function(){
+    $(".hide2").fadeIn();
+    $(".para2").fadeIn();
+  });
+  
