eed1ba5b6bdbb8c599b6940e7934fbc10cbb198b
Patrick Ndonga <rick.ndonga.com>
2019-05-14 01:44:30 +0300

    updating script.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,51 @@ $(document).ready(function(){
  total2 = 0;
  accrueRoll2 = 0;
 
- 
+ $(".roll1").click(function() {
+  randomNo = Math.floor(Math.random() * 6) + 1;
+
+  if (randomNo === 1) {
+    alert("Oops, you rolled a 1");
+    accrueRoll1 = 0;
+    $("#count1").text(accrueRoll1);
+    total1 += accrueRoll1;
+    $("#total1").text(total1);
+    $(".hide1").hide();
+  } else {
+    $("#Roll1").text(randomNo);
+    accrueRoll1 += randomNo;
+    $("#count1").text(accrueRoll1);
+  }
+
+});
+$("#hold1").click(function(){
+   total1 += accrueRoll1;
+   $("#total2").text(totalPoints2);
+   accrueRoll1 = 0;
+   $(".hide1").hide();
+});
+//Player 2//
+$(".roll2").click(function() {
+  randomNo = Math.floor(Math.random() * 6) + 1;
+
+  if (randomNo === 1) {
+    alert("Oops, you rolled a 1");
+    accrueRoll2 = 0;
+    $("#count2").text(accrueRoll2);
+    total2 += accrueRoll2;
+    $("#total2").text(total2);
+    $(".hide2").hide();
+  } else {
+    $("#Roll2").text(randomNo);
+    accrueRoll2 += randomNo;
+    $("#count2").text(accrueRoll2);
+  }
+  $("#hold2").click(function(){
+     total2 += accrueRoll2;
+     $("#total2").text(totalPoints2);
+     accrueRoll2 = 0;
+     $(".hide2").hide();
+  });
+
+});
+});
